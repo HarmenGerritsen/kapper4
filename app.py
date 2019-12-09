ba0224e2ba7b93a99f4876bd2aed5331e450b83f
Harmen <harmen.gerritsen@student.hu.nl>
2019-12-09 14:31:31 +0100

    4.0

--- app.py
+++ app.py
@@ -6,22 +6,21 @@ app = Flask(__name__)
 
 @app.route('/agenda')
 def agenda():
-    base = datetime.datetime.today()
+    base = datetime.date.today()
     output = []
     for x in range(1, 36):
         output.append(base + datetime.timedelta(days=x))
     return render_template('agenda.html', tijd='tijd', maand='maand', dates=output)
 
 
-@app.route('/benis<var>')
+@app.route('/<var>')
 def benis(var):
     print(var)
-    return render_template('succes.html')
+    return render_template('succes.html', datum=var)
 
 
 @app.route('/')
 def index():
-    logger.info('harmen likes bois')
     return render_template('main.html')
 
 
